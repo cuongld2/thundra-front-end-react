1041561b12f62cbc9be5d977f118e859b7af2a8c
Le Dinh Cuong <v.cuongld7@vinbigdata.org>
2021-09-13 22:57:01 +0700

    Add epsagon init

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import Create from './Create';
 import BlogDetails from './BlogDetails';
 import NotFound from './NotFound';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+import epsagon from '@epsagon/web'
 function App() {
+  epsagon.init({
+    token: '89dc771f-b9fe-47e1-871c-ec6be897349c',
+    appName: 'blog-frontend',
+  });
+
+  
   return (
     <Router>
       <div className="App">
